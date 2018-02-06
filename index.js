9137d120867fd44192556b9ae3b0f627ce88213d
ACharLuk <acharluk@gmail.com>
2018-02-06 16:30:25 +0100

    Setup arguments

---
+++ index.js
@@ -0,0 +1,30 @@
+const LunaManager = require('luna-manager').LunaManager;
+
+switch(process.argv[2]) {
+    case '--update':
+        LunaManager.checkForUpdates(process.cwd())
+        break
+    
+    case '--force-update':
+        LunaManager.checkForUpdates(process.cwd(), true)
+        break
+
+    case '--new':
+        LunaManager.newProject(process.cwd())
+        break
+
+    case '--extensions-install':
+        LunaManager.Extensions.updateExtension(process.cwd(), process.argv[3])
+        break
+
+    case '--extensions-update':
+        LunaManager.Extensions.checkInstalledExtensions(process.cwd())
+        break
+
+    case '--extensions-update-force':
+        LunaManager.Extensions.checkInstalledExtensions(process.cwd(), true)
+        break
+
+    default:
+        console.log("No valid arguments!")
+}
