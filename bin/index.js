c6d9b529e0717cbf85fc8e8fee1c61861172ce6a
ACharLuk <acharluk@gmail.com>
2018-02-13 00:01:09 +0100

    Add some security checks

--- bin/index.js
+++ bin/index.js
@@ -5,10 +5,12 @@ const zipFolder = require('zip-folder')
 
 switch(process.argv[2]) {
     case '--update': case '-u':
+        if (!LunaManager.checkCurrentBinariesVersion()) console.error("Luna project not found!"); return;
         LunaManager.checkForUpdates(process.cwd(), console.log)
         break
     
     case '--update-force': case '-uf':
+        if (!LunaManager.checkCurrentBinariesVersion()) console.error("Luna project not found!"); return;
         LunaManager.checkForUpdates(process.cwd(), console.log, true)
         break
 
@@ -17,17 +19,21 @@ switch(process.argv[2]) {
         break
 
     case '--extensions-install': case '-ei':
+        if (!LunaManager.checkCurrentBinariesVersion()) console.error("Luna project not found!"); return;
         LunaManager.updateExtension(process.cwd(), console.log, process.argv[3])
         break
 
     case '--extensions-update': case '-eu':
+        if (!LunaManager.checkCurrentBinariesVersion()) console.error("Luna project not found!"); return;
         LunaManager.checkInstalledExtensions(process.cwd(), console.log)
         break
 
     case '--extensions-update-force': case '-euf':
+        if (!LunaManager.checkCurrentBinariesVersion()) console.error("Luna project not found!"); return;
         LunaManager.checkInstalledExtensions(process.cwd(), console.log, true)
         break
     case '--extensions-remove': case '-er': {
+        if (!LunaManager.checkCurrentBinariesVersion()) console.error("Luna project not found!"); return;
         let extensions = LunaManager.checkFolderForExtensions(process.cwd());
         let extensionsData = [];
         extensions.forEach(e => {
@@ -39,6 +45,7 @@ switch(process.argv[2]) {
     }
     
     case '--extensions-list': case '-el': {
+        if (!LunaManager.checkCurrentBinariesVersion()) console.error("Luna project not found!"); return;
         let extensions = LunaManager.checkFolderForExtensions(process.cwd());
         extensions.forEach(e => {
             let curr = require(LunaManager.getExtensionData(process.cwd(), e));
@@ -61,6 +68,7 @@ switch(process.argv[2]) {
         break
 
     case '--zip': case '-z':
+        if (!LunaManager.checkCurrentBinariesVersion()) console.error("Luna project not found!"); return;
         zipFolder(process.cwd(), process.cwd() + "/luna_game.zip", err => {
             if (err) console.error("Error creating zip file")
             else console.log("Successfully created zip file!")
